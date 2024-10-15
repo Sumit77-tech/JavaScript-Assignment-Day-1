36a00aa6234bd317ca09aa57d05f9474a949934f
SUMIT gourav <sumit.gourav280800@gmail.com>
2024-10-14 20:42:40 -0500

    Third commit

---
+++ task.js
@@ -0,0 +1,7 @@
+let tasks = ["Task 1: Check emails", "Task 2: Team meeting", "Task 3: Code debugging", "Task 4: Make Assignment", "Task 5: Client Presentation"]
+tasks.pop()
+tasks.push("High Priority Task 1")
+tasks.push("High Priority Task 2")
+tasks.pop()
+tasks.push("New Task")
+console.log(tasks)
